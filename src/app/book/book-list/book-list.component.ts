18f4177e72181020885c6c3589e5f7e9f0163f9f
Juan David Roa Moyano <jd.roa@uniandes.edu.co>
2023-11-08 10:35:07 -0500

    Terminado con Reviews

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../book';
+import { BookDetail } from '../book-detail';
 import { BookService } from '../book.service';
 
 @Component({
@@ -9,13 +10,24 @@ import { BookService } from '../book.service';
 })
 export class BookListComponent implements OnInit {
 
-  books: Array<Book> = [];
+  books: Array<BookDetail> = [];
+  selected: Boolean = false;
+  selectedBook!: BookDetail;
+
   constructor(private bookService: BookService) { }
+
   getBooks(): void {
     this.bookService.getBooks().subscribe((books) => {
       this.books = books;
     });
   }
+
+  onSelected(book: BookDetail): void {
+    this.selected = true;
+    this.selectedBook = book;
+  }
+
+
   ngOnInit() {
     this.getBooks();
   }
